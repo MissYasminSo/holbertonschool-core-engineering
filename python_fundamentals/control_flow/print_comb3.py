5ce77e58caa4d93d732569d5cc902635755c8b9b
Yasmin So <yasmin@yasmin.so>
2026-09-21 16:35:42 +1000

    feat: add print_comb3.py

---
+++ python_fundamentals/control_flow/print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+deli = ""
+for i in range(0,10):
+    j = i + 1
+    while (j < 10):
+        print("{:s}{:d}{:d}".format(deli,i,j), end='')
+        deli = ", "
+        j = j + 1
+print("")
