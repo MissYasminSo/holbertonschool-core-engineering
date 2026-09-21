3c083da5cd29901d27e30e7183bcf5cfd1449fa3
Yasmin So <yasmin@yasmin.so>
2026-09-21 18:22:41 +1000

    refactor: negative power case

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -2,8 +2,7 @@
 def pow(a, b):
     res = 1
     for i in range(abs(b)):
-        if (b > 0):
-            res = res * a
-        else:
-            res = res / a
+        res = res * a
+    if b < 0:
+        return 1 / res
     return res
