6e09c180b10f69cab824737a013c88058898d1fd
Yasmin So <yasmin@yasmin.so>
2026-09-21 22:35:07 +1000

    feat: add print_matrix_integer.py

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        delim = ""
+        for element in row:
+            print("{:s}{:d}".format(delim,element), end='')
+            delim = " "
+        print("".format(""))
