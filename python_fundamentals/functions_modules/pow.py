18be58f234e3d5caa0d7ea97ecb5d038109e9fdc
Yasmin So <yasmin@yasmin.so>
2026-09-21 18:17:10 +1000

    feat: negative power

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 def pow(a, b):
     res = 1
-    for i in range(b):
-        res = res * a
+    for i in range(abs(b)):
+        if (b > 0):
+            res = res * a
+        else:
+            res = res / a
     return res
