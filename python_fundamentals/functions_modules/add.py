e1e6fd1cc7210a8e15b4cb15e5954baeb7721a0a
Yasmin So <yasmin@yasmin.so>
2026-09-21 20:21:49 +1000

    refactor: import function

--- python_fundamentals/functions_modules/add.py
+++ python_fundamentals/functions_modules/add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-import add_0 as add
+from add_0 import add
 
 if __name__ == '__main__':
     a = 1
     b = 2
-    print("1 + 2 = {:d}".format(add.add(a, b)))
+    print("1 + 2 = {:d}".format(add(a, b)))
