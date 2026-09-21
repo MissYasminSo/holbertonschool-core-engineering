08b79d199ca13e74bf7ce0ddef8aeec691927e8d
Yasmin So <yasmin@yasmin.so>
2026-09-21 11:47:18 +1000

    style: change tab to spaces :( I miss C already :(

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 number = __import__('random').randint(-10, 10)
 if number > 0:
-	print(number, "is positive")
+    print(number, "is positive")
 elif number < 0:
-	print(number, "is negative")
+    print(number, "is negative")
 else:
-	print(number, "is zero")
+    print(number, "is zero")
